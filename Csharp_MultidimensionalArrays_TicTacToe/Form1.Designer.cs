60184f49e249410186b4054fb606cc77d6ae5b09
Wendy_Lee <ddang727@hotmail.com>
2023-06-26 12:13:07 -0700

    Add project files.

---
+++ Csharp_MultidimensionalArrays_TicTacToe/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Csharp_MultidimensionalArrays_TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            richTextBox1 = new RichTextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Symbol", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(389, 263);
+            button1.Name = "button1";
+            button1.Size = new Size(306, 137);
+            button1.TabIndex = 4;
+            button1.Text = "X/O";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            richTextBox1.Location = new Point(78, 51);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(617, 193);
+            richTextBox1.TabIndex = 5;
+            richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(75, 270);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 6;
+            button2.Text = "1 x 1";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(178, 270);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 7;
+            button3.Text = "1 x 2";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(278, 270);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 8;
+            button4.Text = "1 x 3";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(78, 323);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 29);
+            button5.TabIndex = 9;
+            button5.Text = "2 x 1";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(178, 323);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 29);
+            button6.TabIndex = 10;
+            button6.Text = "2 x 2";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(278, 323);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 11;
+            button7.Text = "2 x 3";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(78, 378);
+            button8.Name = "button8";
+            button8.Size = new Size(94, 29);
+            button8.TabIndex = 12;
+            button8.Text = "3 x 1";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(178, 378);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 29);
+            button9.TabIndex = 13;
+            button9.Text = "3 x 2";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(278, 378);
+            button10.Name = "button10";
+            button10.Size = new Size(94, 29);
+            button10.TabIndex = 14;
+            button10.Text = "3 x 3";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(richTextBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+    }
+}
---
+++ Csharp_MultidimensionalArrays_TicTacToe/Form1.cs
@@ -0,0 +1,527 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.Metadata;
+using System;
+
+namespace Csharp_MultidimensionalArrays_TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        // 1. 3 x 3 matrix
+        // 2. initial turn / turn swap(Button Click Event to change X and Y)
+        // 3. Check Valid Move
+        // 4. Turn Function
+        // 5. Win Check (Win, Lose, Draw)
+        // 6. Reset game
+
+        string[,] MyTicTacToe; // string Array will be shown in richtexbox
+        bool[,] b_selected; // check if selected by player-used for enable
+        int turnCount = 0; // will check if 9 places are checked fully
+        bool b_turn = false;  // false: O's turn, true: X's turn
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            reset_game();
+        }
+
+        void reset_game() // initialize data
+        {
+            MyTicTacToe = new string[,] // value set in memory  
+            {
+                { "  -  ", "  -  ", "  -  " },
+                { "  -  ", "  -  ", "  -  " },
+                { "  -  ", "  -  ", "  -  " }
+            };
+
+            b_selected = new bool[,] {
+                { false, false, false },
+                { false, false, false },
+                { false, false, false }
+            };
+
+            b_turn = false;
+            turnCount = 0;
+
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+            button10.Enabled = true;
+
+            PrintArray(); // Value of MyTicTacToe Array will be printed in RichTextBox
+        }
+
+        void takes_turn()
+        {
+            if (b_turn == true) // If b_turn이 X
+            {
+                b_turn = false; // b_turn이 O
+            }
+            else
+            {
+                b_turn = true;
+            }
+            print_turn();
+        }
+      
+        //    // 상태값을 바꾼다.
+        //    b_turn = !b_turn; // b_turn이 true면 not true(=false)를 b_turn에 넣는다. b_turn이 false면 not false(=true)를 b-turn에 넣는다.
+        //    // 화면에 텍스트를 상태값에 맞게 출력한다.
+
+        void print_turn()
+        {
+            string str = "Who's Turn : ";
+
+            if (b_turn == true)
+            {
+                button1.Text = str + "X";
+            }
+            else
+            {
+                button1.Text = str + "O";
+            }
+
+        }
+
+        void PrintArray(string toFill = "")
+        {
+            richTextBox1.Text = " Tic Tac Toe\n\n";
+            for (int i = 0; i <= MyTicTacToe.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= MyTicTacToe.GetUpperBound(1); j++)
+                {
+                    richTextBox1.Text += MyTicTacToe[i, j];
+                }
+                richTextBox1.Text += "\n";
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PrintArray();
+        }
+
+        private int WinCheck()
+        {
+            if ((MyTicTacToe[0, 0] == " X " && MyTicTacToe[0, 1] == " X " && MyTicTacToe[0, 2] == " X ") ||
+                (MyTicTacToe[1, 0] == " X " && MyTicTacToe[1, 1] == " X " && MyTicTacToe[1, 2] == " X ") ||
+                (MyTicTacToe[2, 0] == " X " && MyTicTacToe[2, 1] == " X " && MyTicTacToe[2, 2] == " X ") ||
+                (MyTicTacToe[0, 0] == " X " && MyTicTacToe[1, 0] == " X " && MyTicTacToe[2, 0] == " X ") ||
+                (MyTicTacToe[0, 1] == " X " && MyTicTacToe[1, 1] == " X " && MyTicTacToe[2, 1] == " X ") ||
+                (MyTicTacToe[0, 2] == " X " && MyTicTacToe[1, 2] == " X " && MyTicTacToe[2, 2] == " X ") ||
+                (MyTicTacToe[0, 0] == " X " && MyTicTacToe[1, 1] == " X " && MyTicTacToe[2, 2] == " X ") ||
+                (MyTicTacToe[0, 2] == " X " && MyTicTacToe[1, 1] == " X " && MyTicTacToe[2, 0] == " X "))
+            {
+                return 0;   // X win
+            }
+            else if
+                ((MyTicTacToe[0, 0] == " O " && MyTicTacToe[0, 1] == " O " && MyTicTacToe[0, 2] == " O ") ||
+                (MyTicTacToe[1, 0] == " O " && MyTicTacToe[1, 1] == " O " && MyTicTacToe[1, 2] == " O ") ||
+                (MyTicTacToe[2, 0] == " O " && MyTicTacToe[2, 1] == " O " && MyTicTacToe[2, 2] == " O ") ||
+                (MyTicTacToe[0, 0] == " O " && MyTicTacToe[1, 0] == " O " && MyTicTacToe[2, 0] == " O ") ||
+                (MyTicTacToe[0, 1] == " O " && MyTicTacToe[1, 1] == " O " && MyTicTacToe[2, 1] == " O ") ||
+                (MyTicTacToe[0, 2] == " O " && MyTicTacToe[1, 2] == " O " && MyTicTacToe[2, 2] == " O ") ||
+                (MyTicTacToe[0, 0] == " O " && MyTicTacToe[1, 1] == " O " && MyTicTacToe[2, 2] == " O ") ||
+                (MyTicTacToe[0, 2] == " O " && MyTicTacToe[1, 1] == " O " && MyTicTacToe[2, 0] == " O "))
+            {
+                return 1;   // O win
+            }
+            else if (turnCount == 9)
+            {
+                return 2;   // draw
+            }
+            else
+            {
+                return 3;   // continue game
+            }
+        }
+
+        string get_turn()
+        {
+            if (b_turn == false)     // false -> O's turn
+            {
+                return " O ";
+            }
+            else
+            {
+                return " X ";
+            }
+        }
+
+        void set_array_value_with_index(int x, int y)
+        {
+            string str = get_turn();
+            MyTicTacToe[x, y] = str;
+            b_selected[x, y] = true; // x, y자리가 지금 눌렸다는 뜻
+        }
+
+        void set_array_value(int button_num) // 
+        {
+            if (button_num == 2)        // 1 x 1
+            {
+                set_array_value_with_index(0, 0);
+                button2.Enabled = false;        // 버튼을 비활성화 한다.
+                // button2.Visible = false;        // 버튼을 사라지게 한다.
+            }
+            else if (button_num == 3)
+            {
+                set_array_value_with_index(0, 1);
+                button3.Enabled = false;
+            }
+            else if (button_num == 4)
+            {
+                set_array_value_with_index(0, 2);
+                button4.Enabled = false;
+            }
+            else if (button_num == 5)
+            {
+                set_array_value_with_index(1, 0);
+                button5.Enabled = false;
+            }
+            else if (button_num == 6)
+            {
+                set_array_value_with_index(1, 1);
+                button6.Enabled = false;
+            }
+            else if (button_num == 7)
+            {
+                set_array_value_with_index(1, 2);
+                button7.Enabled = false;
+            }
+            else if (button_num == 8)
+            {
+                set_array_value_with_index(2, 0);
+                button8.Enabled = false;
+            }
+            else if (button_num == 9)
+            {
+                set_array_value_with_index(2, 1);
+                button9.Enabled = false;
+            }
+            else if (button_num == 10)
+            {
+                set_array_value_with_index(2, 2);
+                button10.Enabled = false;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)  // button2: 1 x 1
+        {
+            if (b_selected[0, 0] == true)
+            {
+                MessageBox.Show("This index is already selected!");
+                return;
+            }
+
+            // wincheck
+            set_array_value(2);
+
+            PrintArray();
+
+            turnCount += 1;
+
+            if (WinCheck() == 0)
+            {
+                MessageBox.Show("X wins!");
+                reset_game();
+                return; //return 은 함수를 종료하겠다는 뜻, 이 지점에서 return으로 종료하지 않으면
+                        //아래 takes_turn() 으로 이어서 진행이 됨. 값을 리턴하면서 종료하면 return 값이 있다는 뜻, 
+            }
+            else if (WinCheck() == 1)
+            {
+                MessageBox.Show("O wins!");
+                reset_game();
+                return;
+            }
+            else if (WinCheck() == 2)
+            {
+                MessageBox.Show($"No one wins You are both losers!!");
+                reset_game();
+                return;
+            }
+
+            takes_turn();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // wincheck
+            set_array_value(3);
+
+            PrintArray();
+
+            turnCount += 1;
+
+            if (WinCheck() == 0)
+            {
+                MessageBox.Show("X wins!");
+                reset_game();
+                return; //return 은 함수를 종료하겠다는 뜻, 이 지점에서 return으로 종료하지 않으면
+                        //아래 takes_turn() 으로 이어서 진행이 됨. 값을 리턴하면서 종료하면 return 값이 있다는 뜻, 
+            }
+            else if (WinCheck() == 1)
+            {
+                MessageBox.Show("O wins!");
+                reset_game();
+                return;
+            }
+            else if (WinCheck() == 2)
+            {
+                MessageBox.Show($"No one wins You are both losers!!");
+                reset_game();
+                return;
+            }
+
+            // button3: 1 x 2
+            takes_turn();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // wincheck
+            set_array_value(4);
+
+            PrintArray();
+
+            turnCount += 1;
+
+            if (WinCheck() == 0)
+            {
+                MessageBox.Show("X wins!");
+                reset_game();
+                return; //return 은 함수를 종료하겠다는 뜻, 이 지점에서 return으로 종료하지 않으면
+                        //아래 takes_turn() 으로 이어서 진행이 됨. 값을 리턴하면서 종료하면 return 값이 있다는 뜻 
+            }
+            else if (WinCheck() == 1)
+            {
+                MessageBox.Show("O wins!");
+                reset_game();
+                return;
+            }
+            else if (WinCheck() == 2)
+            {
+                MessageBox.Show($"No one wins You are both losers!!");
+                reset_game();
+                return;
+            }
+
+            // button4: 1 x 3
+            takes_turn();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // wincheck
+            set_array_value(5);
+
+            PrintArray();
+
+            turnCount += 1;
+
+            if (WinCheck() == 0)
+            {
+                MessageBox.Show("X wins!");
+                reset_game();
+                return; //return 은 함수를 종료하겠다는 뜻, 이 지점에서 return으로 종료하지 않으면
+                        //아래 takes_turn() 으로 이어서 진행이 됨. 값을 리턴하면서 종료하면 return 값이 있다는 뜻, 
+            }
+            else if (WinCheck() == 1)
+            {
+                MessageBox.Show("O wins!");
+                reset_game();
+                return;
+            }
+            else if (WinCheck() == 2)
+            {
+                MessageBox.Show($"No one wins You are both losers!!");
+                reset_game();
+                return;
+            }
+
+            // button5: 2 x 1
+            takes_turn();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // wincheck
+            set_array_value(6);
+
+            PrintArray();
+
+            turnCount += 1;
+
+            if (WinCheck() == 0)
+            {
+                MessageBox.Show("X wins!");
+                reset_game();
+                return; //return 은 함수를 종료하겠다는 뜻, 이 지점에서 return으로 종료하지 않으면
+                        //아래 takes_turn() 으로 이어서 진행이 됨. 값을 리턴하면서 종료하면 return 값이 있다는 뜻, 
+            }
+            else if (WinCheck() == 1)
+            {
+                MessageBox.Show("O wins!");
+                reset_game();
+                return;
+            }
+            else if (WinCheck() == 2)
+            {
+                MessageBox.Show($"No one wins You are both losers!!");
+                reset_game();
+                return;
+            }
+
+            // button6: 2 x 2
+            takes_turn();
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // wincheck
+            set_array_value(7);
+
+            PrintArray();
+
+            turnCount += 1;
+
+            if (WinCheck() == 0)
+            {
+                MessageBox.Show("X wins!");
+                reset_game();
+                return; //return 은 함수를 종료하겠다는 뜻, 이 지점에서 return으로 종료하지 않으면
+                        //아래 takes_turn() 으로 이어서 진행이 됨. 값을 리턴하면서 종료하면 return 값이 있다는 뜻, 
+            }
+            else if (WinCheck() == 1)
+            {
+                MessageBox.Show("O wins!");
+                reset_game();
+                return;
+            }
+            else if (WinCheck() == 2)
+            {
+                MessageBox.Show($"No one wins You are both losers!!");
+                reset_game();
+                return;
+            }
+
+            // button7: 2 x 3
+            takes_turn();
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // 3.wincheck
+            set_array_value(8);
+
+            PrintArray();
+
+            turnCount += 1;
+
+            if (WinCheck() == 0)
+            {
+                MessageBox.Show("X wins!");
+                reset_game();
+                return; //return 은 함수를 종료하겠다는 뜻, 이 지점에서 return으로 종료하지 않으면
+                        //아래 takes_turn() 으로 이어서 진행이 됨. 값을 리턴하면서 종료하면 return 값이 있다는 뜻, 
+            }
+            else if (WinCheck() == 1)
+            {
+                MessageBox.Show("O wins!");
+                reset_game();
+                return;
+            }
+            else if (WinCheck() == 2)
+            {
+                MessageBox.Show($"No one wins You are both losers!!");
+                reset_game();
+                return;
+            }
+
+            // button8: 3 x 1
+            takes_turn();
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // 3.wincheck
+            set_array_value(9);
+
+            PrintArray();
+
+            turnCount += 1;
+
+            if (WinCheck() == 0)
+            {
+                MessageBox.Show("X wins!");
+                reset_game();
+                return; //return 은 함수를 종료하겠다는 뜻, 이 지점에서 return으로 종료하지 않으면
+                        //아래 takes_turn() 으로 이어서 진행이 됨. 값을 리턴하면서 종료하면 return 값이 있다는 뜻, 
+            }
+            else if (WinCheck() == 1)
+            {
+                MessageBox.Show("O wins!");
+                reset_game();
+                return;
+            }
+            else if (WinCheck() == 2)
+            {
+                MessageBox.Show($"No one wins You are both losers!!");
+                reset_game();
+                return;
+            }
+
+            // button9: 3 x 2
+            takes_turn();
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            // wincheck
+            set_array_value(10);
+
+            PrintArray();
+
+            turnCount += 1;
+
+            if (WinCheck() == 0)
+            {
+                MessageBox.Show("X wins!");
+                reset_game();
+                return; //return은 함수를 종료하겠다는 뜻, 이 지점에서 return으로 종료하지 않으면
+                        //아래 takes_turn() 으로 이어서 진행이 됨. 값을 리턴하면서 종료하면 return 값이 있다는 뜻, 
+            }
+            else if (WinCheck() == 1)
+            {
+                MessageBox.Show("O wins!");
+                reset_game();
+                return;
+            }
+            else if (WinCheck() == 2)
+            {
+                MessageBox.Show($"No one wins You are both losers!!");
+                reset_game();
+                return;
+            }
+
+            // button10: 3 x 3
+            takes_turn();
+
+        }
+    }
+}
